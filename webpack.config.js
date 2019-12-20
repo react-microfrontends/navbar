a2de12b6cf1aa3ed808ba5d3189fa44af32973d8
Joel Denning <joeldenning@gmail.com>
2019-12-20 15:35:07 -0700

    Switching over to using create-single-spa

--- webpack.config.js
+++ webpack.config.js
@@ -1,96 +1,14 @@
-/* eslint-env node */
-const webpack = require('webpack')
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/navbar.js'),
-  output: {
-    filename: 'navbar.js',
-    library: 'navbar',
-    libraryTarget: 'amd',
-    path: path.resolve(__dirname, 'dist/navbar'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {parser: {System: false}},
-      {
-        test: /\.js?$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        exclude: [path.resolve(__dirname, 'node_modules'), /\.krem.css$/],
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]',
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins() {
-                return [
-                  require('autoprefixer')
-                ];
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        include: [path.resolve(__dirname, 'node_modules')],
-        exclude: [/\.krem.css$/],
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.krem.css$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        use: [
-          {
-            loader: 'kremling-loader',
-            options: {
-              namespace: 'app-dashboard-ui',
-              postcss: {
-                plugins: {
-                  'autoprefixer': {}
-                }
-              }
-            },
-          },
-        ]
-      },
-    ],
-  },
-  resolve: {
-    modules: [
-      __dirname,
-      'node_modules',
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist/navbar']),
-    CopyWebpackPlugin([
-      {from: path.resolve(__dirname, 'src/navbar.js')}
-    ]),
-  ],
-  devtool: 'source-map',
-  externals: [
-    /^lodash$/,
-    /^single-spa$/,
-    /^react$/,
-    /^react\/lib.*/,
-    /^react-dom$/,
-    /.*react-dom.*/,
-    /^rxjs\/?.*$/,
-  ],
+module.exports = webpackConfigEnv => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "react-mf",
+    projectName: "nav-bar",
+    webpackConfigEnv
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
 };
-
