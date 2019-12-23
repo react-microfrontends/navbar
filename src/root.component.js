3a9c645a6601d7a779a183add946ba11e6a40c9f
Joel Denning <joeldenning@gmail.com>
2019-12-23 16:42:44 -0700

    Switching over to using create-single-spa (#2)
    * Switching over to using create-single-spa
    
    * Self review
    
    * Fixes

--- src/root.component.js
+++ src/root.component.js
@@ -1,46 +1,5 @@
-import React from 'react'
-import { Scoped } from 'kremling'
-import styles from './root.krem.css'
-import { links } from './root.helper.js'
-import { Link } from '@reach/router'
+import React from "react";
 
-export default class Root extends React.Component {
-
-  state = {
-    hasError: false
-  }
-
-  componentDidCatch (error, info) {
-    this.setState({hasError: true})
-  }
-
-  render () {
-    return (
-      <Scoped css={styles}>
-        {
-          this.state.hasError ? (
-            <div className='root navBarHeight'>
-              Error
-            </div>
-          ) : (
-            <div className='root navBarHeight'>
-              {
-                links.map((link) => {
-                  return (
-                    <Link
-                      key={link.href}
-                      className='primary-navigation-link'
-                      to={link.href}
-                    >
-                      {link.name}
-                    </Link>
-                  )
-                })
-              }
-            </div>
-          )
-        }
-      </Scoped>
-    )
-  }
+export default function Root(props) {
+  return <section>{props.name} is mounted!</section>;
 }
--- src/root.helper.js
+++ src/root.helper.js
@@ -1,10 +1,10 @@
 export const links = [
   {
-    name: 'People',
-    href: '/people'
+    name: "People",
+    href: "/people"
   },
   {
-    name: 'Planets',
-    href: '/planets'
+    name: "Planets",
+    href: "/planets"
   }
-]
+];
