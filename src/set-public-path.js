f2cc1fe14079b5d2ae94a5b08202f64d9b3e33dd
Joel Denning <joeldenning@gmail.com>
2019-12-23 18:27:16 -0700

    Bringing over Navbar code

--- src/set-public-path.js
+++ src/set-public-path.js
@@ -1,3 +1,15 @@
-import { setPublicPath } from "systemjs-webpack-interop";
+// For lazy loading within an application to work you need to set webpack's public path
+// basically webpack's internal module system always looks for code-splits (modules) at the root
+export default function setPublicPath() {
+  return Promise.all([getUrl()]).then(values => {
+    const [url] = values;
+    const webpackPublicPath = url.slice(0, url.lastIndexOf("/") + 1);
 
-setPublicPath("@react-mf/navbar");
+    __webpack_public_path__ = webpackPublicPath;
+    return true;
+  });
+}
+
+function getUrl() {
+  return window.System.resolve("@portal/people");
+}
