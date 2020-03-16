9d9ce4e1e51cacb244b9f5a00a6c32226b9660b2
Joel Denning <joeldenning@gmail.com>
2020-03-16 16:26:32 -0600

    Correctly setting webpack public path

--- src/set-public-path.js
+++ src/set-public-path.js
@@ -1,15 +1,3 @@
-// For lazy loading within an application to work you need to set webpack's public path
-// basically webpack's internal module system always looks for code-splits (modules) at the root
-export default function setPublicPath() {
-  return Promise.all([getUrl()]).then(values => {
-    const [url] = values;
-    const webpackPublicPath = url.slice(0, url.lastIndexOf("/") + 1);
+import { setPublicPath } from "systemjs-webpack-interop";
 
-    __webpack_public_path__ = webpackPublicPath;
-    return true;
-  });
-}
-
-function getUrl() {
-  return window.System.resolve("@portal/people");
-}
+setPublicPath("@react-mf/navbar");
