0a2ead73b6eb34bc61057506566f1be7501083be
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-19 17:37:48 -0600

    removing kremling

--- src/root.component.js
+++ src/root.component.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Scoped } from "kremling";
-import styles from "./root.krem.css";
 import { links } from "./root.helper.js";
 import { Link } from "@reach/router";
 
@@ -14,40 +12,42 @@ export default class Root extends React.Component {
   }
 
   render() {
-    return (
-      <Scoped css={styles}>
-        {this.state.hasError ? (
-          <div className="root navBarHeight">Error</div>
-        ) : (
-          <div className="root navBarHeight flex items-center justify-between px-6 bg-primary">
-            <div className="flex items-center justify-between">
-              {links.map(link => {
-                return (
-                  <Link
-                    key={link.href}
-                    className="p-6 text-contrast text-secondary:hover"
-                    to={link.href}
-                  >
-                    {link.name}
-                  </Link>
-                );
-              })}
-            </div>
-            <div className="flex items-center justify-between">
-              <button className="mr-4" onClick={switchTheme}>
-                Switch theme
-              </button>
-              <a
-                href="https://github.com/react-microfrontends"
-                className="externalLink"
-              >
-                Github project
-              </a>
-            </div>
+    if (this.state.hasError) {
+      return (
+        <div className="h-16 flex items-center justify-between px-6 bg-danger">
+          Error
+        </div>
+      );
+    } else {
+      return (
+        <div className="h-16 flex items-center justify-between px-6 bg-primary">
+          <div className="flex items-center justify-between">
+            {links.map(link => {
+              return (
+                <Link
+                  key={link.href}
+                  className="p-6 text-contrast text-secondary:hover"
+                  to={link.href}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
           </div>
-        )}
-      </Scoped>
-    );
+          <div className="flex items-center justify-between">
+            <button className="mr-4" onClick={switchTheme}>
+              Switch theme
+            </button>
+            <a
+              href="https://github.com/react-microfrontends"
+              className="externalLink"
+            >
+              Github project
+            </a>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
