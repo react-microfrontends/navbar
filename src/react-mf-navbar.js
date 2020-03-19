560b3db3710862431ee3d45377374d7250dbbe0c
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-19 14:37:50 -0600

    switching to use a nav node instead of a div (#6)
    * switching to use a nav node instead of a div
    
    * removing setTimeout
    
    * removing more

--- src/react-mf-navbar.js
+++ src/react-mf-navbar.js
@@ -7,9 +7,20 @@ import Root from "./root.component";
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: Root
+  rootComponent: Root,
+  domElementGetter
 });
 
 export const bootstrap = lifecycles.bootstrap;
 export const mount = lifecycles.mount;
 export const unmount = lifecycles.unmount;
+
+function domElementGetter() {
+  let el = document.getElementById("navbar");
+  if (!el) {
+    el = document.createElement("nav");
+    el.id = "navbar";
+    document.body.appendChild(el);
+  }
+  return el;
+}
