e3fc876857ddefd93376434a7df94b987451bfac
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-20 11:42:42 -0600

    omiting brand stuff for now

--- src/root.component.js
+++ src/root.component.js
@@ -14,30 +14,23 @@ export default class Root extends React.Component {
   render() {
     if (this.state.hasError) {
       return (
-        <div className="h-16 flex items-center justify-between px-6 bg-danger">
+        <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
           Error
         </div>
       );
     } else {
       return (
-        <div className="h-16 flex items-center justify-between px-6 bg-primary">
+        <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
           <div className="flex items-center justify-between">
             {links.map(link => {
               return (
-                <Link
-                  key={link.href}
-                  className="p-6 text-contrast text-secondary:hover"
-                  to={link.href}
-                >
+                <Link key={link.href} className="p-6" to={link.href}>
                   {link.name}
                 </Link>
               );
             })}
           </div>
           <div className="flex items-center justify-between">
-            <button className="mr-4" onClick={switchTheme}>
-              Switch theme
-            </button>
             <a
               href="https://github.com/react-microfrontends"
               className="externalLink"
@@ -49,87 +42,4 @@ export default class Root extends React.Component {
       );
     }
   }
-}
-
-function switchTheme() {
-  const computed = getComputedStyle(document.documentElement);
-  const imperialPrimary = computed
-    .getPropertyValue("--imperial-primary")
-    .trim();
-  const currentPrimary = computed.getPropertyValue("--color-primary").trim();
-  if (imperialPrimary === currentPrimary) {
-    setRebelTheme();
-  } else {
-    setImperialTheme();
-  }
-}
-
-function setRebelTheme() {
-  document.documentElement.style.setProperty(
-    "--color-primary",
-    "var(--rebel-primary)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-secondary",
-    "var(--rebel-red)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-contrast",
-    "var(--rebel-white)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-success",
-    "var(--rebel-green)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-danger",
-    "var(--imperial-primary)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-warning",
-    "var(--rebel-yellow)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-info",
-    "var(--rebel-blue)"
-  );
-  document.documentElement.style.setProperty(
-    "--background",
-    "var(--rebel-white)"
-  );
-}
-
-function setImperialTheme() {
-  document.documentElement.style.setProperty(
-    "--color-primary",
-    "var(--imperial-primary)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-secondary",
-    "var(--imperial-red)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-contrast",
-    "var(--imperial-white)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-danger",
-    "var(--rebel-primary)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-success",
-    "var(--imperial-green)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-warning",
-    "var(--imperial-yellow)"
-  );
-  document.documentElement.style.setProperty(
-    "--color-info",
-    "var(--imperial-blue)"
-  );
-  document.documentElement.style.setProperty(
-    "--background",
-    "var(--imperial-black)"
-  );
 }
