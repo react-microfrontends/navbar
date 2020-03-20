74c8fe82d1cc6968451ec1293303499e632d48ef
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-20 12:14:49 -0600

    Tailwind (#9)
    * adding some basic theming
    
    * removing kremling
    
    * basic theme switching
    
    * omiting brand stuff for now

--- src/root.component.js
+++ src/root.component.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Scoped } from "kremling";
-import styles from "./root.krem.css";
 import { links } from "./root.helper.js";
 import { Link } from "@reach/router";
 
@@ -14,36 +12,34 @@ export default class Root extends React.Component {
   }
 
   render() {
-    return (
-      <Scoped css={styles}>
-        {this.state.hasError ? (
-          <div className="root navBarHeight">Error</div>
-        ) : (
-          <div className="root navBarHeight">
-            <div>
-              {links.map(link => {
-                return (
-                  <Link
-                    key={link.href}
-                    className="primary-navigation-link"
-                    to={link.href}
-                  >
-                    {link.name}
-                  </Link>
-                );
-              })}
-            </div>
-            <div>
-              <a
-                href="https://github.com/react-microfrontends"
-                className="externalLink"
-              >
-                Github project
-              </a>
-            </div>
+    if (this.state.hasError) {
+      return (
+        <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
+          Error
+        </div>
+      );
+    } else {
+      return (
+        <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
+          <div className="flex items-center justify-between">
+            {links.map(link => {
+              return (
+                <Link key={link.href} className="p-6" to={link.href}>
+                  {link.name}
+                </Link>
+              );
+            })}
           </div>
-        )}
-      </Scoped>
-    );
+          <div className="flex items-center justify-between">
+            <a
+              href="https://github.com/react-microfrontends"
+              className="externalLink"
+            >
+              Github project
+            </a>
+          </div>
+        </div>
+      );
+    }
   }
 }
