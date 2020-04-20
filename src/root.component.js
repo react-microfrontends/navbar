a6839022281b557ce085a72ff4be128c133554c6
Joel Denning <joeldenning@gmail.com>
2020-04-20 13:15:42 -0600

    Upgrading to single-spa-react@2.14.0 (#12)

--- src/root.component.js
+++ src/root.component.js
@@ -2,44 +2,26 @@ import React from "react";
 import { links } from "./root.helper.js";
 import { Link } from "@reach/router";
 
-export default class Root extends React.Component {
-  state = {
-    hasError: false
-  };
-
-  componentDidCatch(error, info) {
-    this.setState({ hasError: true });
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
-          Error
-        </div>
-      );
-    } else {
-      return (
-        <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
-          <div className="flex items-center justify-between">
-            {links.map(link => {
-              return (
-                <Link key={link.href} className="p-6" to={link.href}>
-                  {link.name}
-                </Link>
-              );
-            })}
-          </div>
-          <div className="flex items-center justify-between">
-            <a
-              href="https://github.com/react-microfrontends"
-              className="externalLink"
-            >
-              Github project
-            </a>
-          </div>
-        </div>
-      );
-    }
-  }
+export default function Root(props) {
+  return (
+    <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
+      <div className="flex items-center justify-between">
+        {links.map(link => {
+          return (
+            <Link key={link.href} className="p-6" to={link.href}>
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="flex items-center justify-between">
+        <a
+          href="https://github.com/react-microfrontends"
+          className="externalLink"
+        >
+          Github project
+        </a>
+      </div>
+    </div>
+  );
 }
