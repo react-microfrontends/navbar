2bc23524cf31f078c8d32a255264ab5d0b02ec62
Joel Denning <joeldenning@gmail.com>
2024-10-01 23:53:46 -0600

    Switch to ESM format, switch to react-router

--- src/root.component.js
+++ src/root.component.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { links } from "./root.helper.js";
-import { Link } from "@reach/router";
+import { Link, BrowserRouter } from "react-router-dom";
 
 export default function Root(props) {
   return (
-    <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
-      <div className="flex items-center justify-between">
-        {links.map((link) => {
-          return (
-            <Link key={link.href} className="p-6" to={link.href}>
-              {link.name}
-            </Link>
-          );
-        })}
+    <BrowserRouter>
+      <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
+        <div className="flex items-center justify-between">
+          {links.map((link) => {
+            return (
+              <Link key={link.href} className="p-6" to={link.href}>
+                {link.name}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="flex items-center justify-between">
+          <a
+            href="https://github.com/react-microfrontends"
+            className="externalLink"
+          >
+            Github project
+          </a>
+        </div>
       </div>
-      <div className="flex items-center justify-between">
-        <a
-          href="https://github.com/react-microfrontends"
-          className="externalLink"
-        >
-          Github project
-        </a>
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
